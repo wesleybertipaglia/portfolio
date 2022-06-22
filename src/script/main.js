e1dc2a642489ec292e841d2560a77cf508c0abad
Wesley <wesleybertipaglia@gmail.com>
2022-06-22 14:02:50 -0300

    feat: SEO implementation

---
+++ src/script/main.js
@@ -0,0 +1,15 @@
+const toTopButton = document.getElementById("button-to-top");
+window.onscroll = function() {scrollFunction()};
+        
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        toTopButton.style.display = "block";
+    } else {
+        toTopButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
