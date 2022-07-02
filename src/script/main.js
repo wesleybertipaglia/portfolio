5b1cf7af8bda35825c4cb348d641974a603c801e
Wesley Bertipaglia <49965963+wesleybertipaglia@users.noreply.github.com>
2022-07-02 11:18:36 -0300

    Feat: add smooth animation for to top function
    Refactor the to top function using window.scroll and add behavior smooth animation

--- src/script/main.js
+++ src/script/main.js
@@ -1,15 +1,14 @@
 const toTopButton = document.getElementById("button-to-top");
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()}
         
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        toTopButton.style.display = "block";
+        toTopButton.style.display = "block"
     } else {
-        toTopButton.style.display = "none";
+        toTopButton.style.display = "none"
     }
 }
 
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    window.scroll({top: 0, left: 0, behavior: 'smooth'})
 }
