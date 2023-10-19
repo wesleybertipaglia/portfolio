ba40d6da2b143c742fb2330f4d33316099c1616a
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-10-19 00:32:22 -0300

    feat: add header and footer

---
+++ lib/data.ts
@@ -0,0 +1,86 @@
+export const links = [
+    {
+        name: 'Hero',
+        url: '#hero'
+    },
+    {
+        name: 'About',
+        url: '#about'
+    },
+    {
+        name: 'Projects',
+        url: '#projects'
+    },
+    {
+        name: 'Skills',
+        url: '#skills'
+    },
+    {
+        name: 'Experience',
+        url: '#experience'
+    },
+    {
+        name: 'Contact',
+        url: '#contact'
+    },
+] as const;
+
+export const experiencesData = [
+    {
+        title: 'Freelancer Web Developer',
+        location: 'São José do Rio Preto - SP, Brazil',
+        description: '',
+        date: '2021 - 2022'        
+    },    
+    {
+        title: 'Web Developer Intern',
+        location: 'São José do Rio Preto - SP, Brazil',
+        description: '',
+        date: '2022 - 2023'        
+    },
+    {
+        title: 'Full-Stack Web Developer',
+        location: 'São José do Rio Preto - SP, Brazil',
+        description: '',
+        date: '2023 - present'        
+    },
+] as const;
+
+export const projectsData = [
+    {
+        title: 'Cozzy Store',
+        description: '',
+        tags: ['PHP', 'MySql', 'JS', 'SASS'],
+        imageUrl: ''
+    },
+    {
+        title: 'Hub of Links',
+        description: '',
+        tags: ['React', 'Tailwind', 'JS'],
+        imageUrl: ''
+    },
+    {
+        title: 'Wildlife Preservation',
+        description: '',
+        tags: ['React', 'Tailwind', 'JS', 'Postgree'],
+        imageUrl: ''
+    },
+] as const;
+
+export const skillsData = [
+    'HTML',
+    'CSS',
+    'Saas',
+    'Bootstrap',
+    'Tailwind',
+    'Git',
+    'PHP',
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'Next.js',
+    'Node.js',
+    'Express',
+    'MySql',
+    'PostgreSQL',
+]
