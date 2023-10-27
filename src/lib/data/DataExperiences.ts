41740c607f2a51e4c63dae0632272fea1b42521d
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-10-27 13:21:44 -0300

    feat: change data dir

---
+++ src/lib/data/DataExperiences.ts
@@ -0,0 +1,26 @@
+export const Experiences = [
+    {
+        title: 'Freelancer Web Developer',
+        company: 'Self-Employed',
+        location: 'São José do Rio Preto, SP, Brazil',
+        description: 'As a freelance web developer, I worked on various web projects, creating and maintaining websites using a stack that included HTML, CSS, SASS, Bootstrap, JavaScript, PHP, Wordpress, and designing with Figma.',
+        date: 'January 2021 - June 2022',
+        techs: ['HTML', 'CSS', 'SASS', 'Bootstrap', 'JavaScript', 'PHP', 'MySQL', 'MariaDB', 'Wordpress', 'Figma']     
+    },    
+    {
+        title: 'Web Developer Intern',
+        company: 'AnCode App',
+        location: 'São José do Rio Preto, SP, Brazil',
+        description: 'As a web developer intern at AnCode App, I contributed to web development projects, gaining experience in front-end and back-end technologies. I used HTML, CSS, SASS, Bootstrap, JavaScript, PHP, Wordpress for front-end development, and .NET and C# for back-end development.',
+        date: 'July 2022 - April 2023',
+        techs: ['HTML', 'CSS', 'SASS', 'Bootstrap', 'JavaScript', 'PHP', 'Wordpress', 'Figma', '.NET', 'C#', 'Blazor']
+    },
+    {
+        title: 'Full-Stack Web Developer',
+        company: 'AnCode App',
+        location: 'São José do Rio Preto, SP, Brazil',
+        description: 'As a full-stack web developer at AnCode App, I am responsible for developing and maintaining web applications. My tech stack includes .NET, C#, Blazor, PostgreSQL, Bootstrap, SASS, JavaScript, and HTML.',
+        date: 'April 2023 - Present',
+        techs: ['.NET', 'C#','Blazor', 'Postgree', 'Bootstrap', 'SASS', 'JavaScript']        
+    },
+] as const;
